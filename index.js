85e726d2c658945596ce708c62f1ecc86d835d95
kuraiokamiblaze <kuraiokamiblaze@penguin>
2022-01-18 13:56:33 -0700

    MVP Task 3 Done_3/6 Passed Tests

--- index.js
+++ index.js
@@ -4,26 +4,29 @@ const { fifaData } = require('./fifa.js')
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
+
+//(a) Home Team name for 2014 world cup final
+
+//(b) Away Team name for 2014 world cup final
+
+//(c) Home Team goals for 2014 world cup final
+
+//(d) Away Team goals for 2014 world cup final
+
+//(e) Winner of 2014 world cup final */
+
+//Data filter
 const finals2014 = fifaData.filter(function(item){
     return item.Year === 2014 && item.Stage === 'Final';
 });
 
-console.log('Task 1', finals2014)
-
-//(a) Home Team name for 2014 world cup final
-console.log('Task 1a', finals2014[0]['Home Team Name']);
-
-//(b) Away Team name for 2014 world cup final
-console.log('Task 1b', finals2014[0]['Away Team Name']);
-
-//(c) Home Team goals for 2014 world cup final
-console.log('Task 1c', finals2014[0]['Home Team Goals']);
-
-//(d) Away Team goals for 2014 world cup final
-console.log('Task 1d', finals2014[0]['Away Team Goals']);
-
-//(e) Winner of 2014 world cup final */
-console.log('Task 1e',finals2014[0]['Win conditions']);
+// Task 1_Console.log(s)
+// console.log('Task 1', finals2014)
+// console.log('Task 1a', finals2014[0]['Home Team Name']);
+// console.log('Task 1b', finals2014[0]['Away Team Name']);
+// console.log('Task 1c', finals2014[0]['Home Team Goals']);
+// console.log('Task 1d', finals2014[0]['Away Team Goals']);
+// console.log('Task 1e',finals2014[0]['Win conditions']);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
@@ -39,21 +42,24 @@ function getFinals(array) {
    });
    return newArray //belongs to getFinals
 }
-
-// Task Passed
+// Task 2_Console.log(s): Passed
 // console.log('Task 2', getFinals(fifaData));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function called getYears to do the following: 
-1. Receive an array
+1. Receive an array //fifaData
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(array, cb) {
+    const cbArr = cb(array);
+    const years = cbArr.map(function(item){
+        return item.Year //Belongs to map
+    });
+    return years //belongs to getYears
 }
-
-
+// Task 3_Console.log(s): Passed
+// console.log('Task 3', getYears(fifaData,getFinals));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function getWinners to do the following:  
